ebdccb2b6b19c0b84de5142a5319b91b016b6d0f
arnarfreyrg <arnarfreyrg@gmail.com>
2018-12-05 12:42:12 +0000

    create order

--- models/order.py
+++ models/order.py
@@ -4,7 +4,7 @@ from models.customer import customer
 class order:
     #initalizing order instance
     #vantar að tengja carID og customerID inn í orders
-    def __init__(self, orderID, carID, customerID, dateOfHandover, returnDate, orderTotal):
+    def __init__(self, orderID,dateOfHandover, returnDate, orderTotal):
         self.__orderID = orderID
         # self.__carID = car.getcarID()
         # self.__customerID = customer.getSSN()
@@ -13,16 +13,16 @@ class order:
         self.__orderTotal = orderTotal
 
     def __str__(self):
-        return (self.__orderID, self.__carID, self.__customerID,self.__dateOfHandover, self.__returnDate)
+        return (self.__orderID,self.__dateOfHandover, self.__returnDate)
 
     def get_orderID(self):
         return self.__orderID
 
-    def get_carID(self):
-        return self.__carID
+    #def get_carID(self):
+        #return self.__carID
 
-    def get_customerID(self):
-        return self.__customerID
+    #def get_customerID(self):
+        #return self.__customerID
 
     def get_dateOfHandover(self):
         return self.__dateOfHandover
--- repositories/orderRepo.py
+++ repositories/orderRepo.py
@@ -7,19 +7,17 @@ class orderRepo():
     def add_order(self,order):
         with open ("./data/orders.txt", "a+") as orders_file:
             orderID = order.get_orderID()
-            carID = order.get_carID()
-            customerID = order.get_customerID()
             dateOfHandover = order.get_dateOfHandover()
             returnDate = order.get_returnDate()
             orderTotal = order.get_orderTotal()
-            orders_file.write("{},{},{},{},{},{}\n".format(orderID, carID, customerID, dateOfHandover, returnDate, orderTotal))
+            orders_file.write("{},{},{},{}\n".format(orderID,dateOfHandover, returnDate, orderTotal))
 
     def get_order(self):
         if self.__order == []:
             with open ("./data/orders.txt", "r") as orders_file:
                 for line in orders_file.readlines():
-                    orderID, carID, customerID, dateOfHandover, returnDate, orderTotal = line.split(",")
-                    all_orders = order(orderID, carID, customerID, dateOfHandover, returnDate, orderTotal)
+                    orderID, dateOfHandover, returnDate, orderTotal = line.split(",")
+                    all_orders = order(orderID, dateOfHandover, returnDate, orderTotal)
                     self.__order.append(all_orders)
                     
         return self.__order
