dc973b224c4e8188f1106b7b20ab3568051a154b
Björn Þorláksson <bjornt17@ru.is>
2018-12-05 12:09:31 +0000

    s
    order

--- ui/mainMenuUI.py
+++ ui/mainMenuUI.py
@@ -1,10 +1,12 @@
 from ui.carUI import carUI
 from ui.customerUi import customerUI
-# from ui.orderUi import orderUi
+from ui.orderUi import orderUI
 import os
 
 
 class mainMenuUI():
+    def __init__(self):
+        self.__carUI = carUI()
 
     def menu(self):
         choice = ""
@@ -16,15 +18,15 @@ class mainMenuUI():
             print("Press q to quit")
             choice = input("choose an option: ").lower()
             clear()
+
             if choice == "1":
                 ui = carUI()
                 ui.menu()
-            elif choice == "2":
+
+            if choice == "2":
                 ui2 = customerUI()
                 ui2.menu()
 
-            elif choice == "3":
+            if choice == "3":
                 ui3 = orderUI()
                 ui3.menu()
-            else:
-                print("please try again")
