08695b17c8f4ae318b43dcfb7715b065ac505153
Alvarlegur <halldoralvar@gmail.com>
2018-12-06 15:50:06 +0000

    asd

--- models/car.py
+++ models/car.py
@@ -8,7 +8,7 @@ class car:
         self.__fuelType = fuelType
         self.__priceGroup = priceGroup
         self.__manufYear = manufYear
-
+        self.__availability = availability
 
     def __str__(self):
         return ("\t{}\n\t{}\t\t{}\t\t{}\t\t{}\t\t{}\t\t{}\t\t{}\n".format("License Plate\tManufacturer\tType Car\tManual or Auto\tFuel Type\tPrice Group\tManufacturer Year\t",self.__licensePlate, self.__manufacturer,self.__typeCar,self.__manOrAuto,self.__fuelType,self.__priceGroup,self.__manufYear))
@@ -34,6 +34,19 @@ class car:
     def get_fuelType(self):
         return self.__fuelType
 
+    def get_availability(self):
+        return self.__availability
+
     def __repr__(self):
         return self.__str__()
+
+    def set_licensePlate(self):
+        self.__licensePlate = input("Input new license plate: ")
+    
+    def set_manufacturer(self):
+        self.__manufacturer = input("Input new manufacturer: ")
+
+    def set_typeCar(self):
+        self.__typeCar = input("Input new car type: ")
+    
     
--- models/order.py
+++ models/order.py
@@ -6,11 +6,7 @@ from models.priceGroup import priceGroup
 class order:
     #initalizing order instance
     #vantar að tengja carID og customerID inn í orders
-<<<<<<< HEAD
-    def __init__(self, licensePlate, customerID, orderID,dateOfHandover, returnDate, orderTotal):
-=======
     def __init__(self, orderID,dateOfHandover, returnDate, extraInsurance, orderTotal):
->>>>>>> bdeb405496182b652465b4736838a7e61033fbd4
         self.__orderID = id(orderID)
         self.__carID = licensePlate
         self.__customerID = customerID
@@ -18,6 +14,7 @@ class order:
         self.__returnDate = returnDate
         self.__extrainsurance = extraInsurance
         self.__orderTotal = orderTotal
+
 
     def __str__(self):
         return "{}\t{}\t{}\t{}\t{} \n".format(self.__orderID,self.__dateOfHandover, self.__returnDate, self.__extrainsurance, self.__orderTotal)
