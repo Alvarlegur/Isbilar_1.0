a28a1a7ee32aa8e9e55126d42d92295687bff80f
arnarfreyrg <arnarfreyrg@gmail.com>
2018-12-10 14:35:39 +0000

    sma breyting a car rep

--- repositories/CarRepo.py
+++ repositories/CarRepo.py
@@ -8,7 +8,7 @@ class CarRepo():
         self.__cars = []
 
     def add_car(self,car):
-        with open ("./data/cars.txt","a+") as cars_file:
+        with open ("./data/cars.csv","a+") as cars_file:
             licensePlate = car.get_licensePlate()
             manufacturer = car.get_manufacturer()
             typeCar = car.get_typeCar()
@@ -16,7 +16,8 @@ class CarRepo():
             fuelType = car.get_fuelType()
             priceGroup = car.get_priceGroup()
             manufYear = car.get_manufYear()
-            cars_file.write("{}, {}, {}, {}, {}, {}, {}\n".format(licensePlate, manufacturer,typeCar,manOrAuto,fuelType,priceGroup,manufYear))
+            availability = car.get_availability()
+            cars_file.write("{}, {}, {}, {}, {}, {}, {},{}\n".format(licensePlate, manufacturer,typeCar,manOrAuto,fuelType,priceGroup,manufYear,availability))
 
     def availability(self):
         with open('./data/cars.csv','r+') as inp, open('./data/availablecars.csv', 'w') as out1, open('./data/unavailablecars.csv','w') as out2:
@@ -29,24 +30,24 @@ class CarRepo():
                     writer2.writerow(row)
 
     def get_AvailCars(self):
-        if self.__cars == []:
-            with open('./data/availablecars.csv','r') as laust:
-                reader = csv.reader(laust)
-                for row in reader:
-                    self.__cars.append(row)
+        self.__cars = []
+        with open('./data/availablecars.csv','r') as laust:
+            reader = csv.reader(laust)
+            for row in reader:
+                self.__cars += row
         return self.__cars
 
     def get_UnavailCars(self):
-        if self.__cars == []:
-            with open('.data/unavailablecars.csv','r') as tekid:
-                reader = csv.reader(tekid)
-                for row in reader:
-                    self.__cars.append(row)
-        return self.__cars
+        unavailCars = []
+        with open('./data/unavailablecars.csv','r') as tekid:
+            reader = csv.reader(tekid)
+            for row in reader:
+                unavailCars += row
+        return unavailCars[8:]
 
     def delete_car(self):
         entername = str(input("Enter cars license plate: "))
-        with open("./data/cars.txt", "r+") as cars_file:
+        with open("./data/cars.csv", "r+") as cars_file:
             temp = cars_file.readlines()
             cars_file.seek(0)
             for line in temp:
