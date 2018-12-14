a5f9b4227e67db4d556456a5397beb651eac9bcd
Björn Þorláksson <bjornt17@ru.is>
2018-12-14 14:44:25 +0000

    1
    Customer repo input checkað

--- services/inputCheck.py
+++ services/inputCheck.py
@@ -1,15 +1,19 @@
-# def checkSSN():
-#     entername = str(input("Enter customers SSN: "))
-#     while entername.isalnum() != True and len(entername) != 10:
-#         print("Social security number has to be all numbers and with length 10\nPlease try again.")
-#         entername = str(input("Enter customers SSN: "))
 
 def checkSSN():
-    
-    entername = input("Enter customers SSN: ")
-    while entername.isalnum() == False and len(entername) != 10:
+    entername = str(input("Enter customers SSN: "))
+    while entername.isdigit() != True and len(entername) != 10:
         print("Social security number has to be all numbers and with length 10\nPlease try again.")
-        entername = input("Enter customers SSN: ")
+        entername = str(input("Enter customers SSN: "))
     return entername
+
+
+def checkPassportID():
+    passport = str(input("Enter customers passport ID: "))
+    while len(passport) != 8:
+        print("Passport ID must contain 8 letters.")
+        passport = str(input("Enter a change of passport ID: "))
+    return passport
+
+        
 
     
